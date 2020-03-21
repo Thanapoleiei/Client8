58879fb0ecf9317c5fd7d4a41818fc6ddf01a38a
Thanapoleiei <loveyouna089@gamil.com>
2020-03-22 00:17:41 +0700

    config

---
+++ src/config.js
@@ -0,0 +1,11 @@
+const config = {
+apiKey: "AIzaSyAAIILh5NHRp4Of9mTqesoz4OUsgUDNQQk",
+  authDomain: "lab8-f153f.firebaseapp.com",
+  databaseURL: "https://lab8-f153f.firebaseio.com",
+  projectId: "lab8-f153f",
+  storageBucket: "lab8-f153f.appspot.com",
+  messagingSenderId: "521121613646",
+  appId: "1:521121613646:web:866bd1103aa8fd8bf0401f",
+  measurementId: "G-WQTFBTTFDH"
+  };
+export default config;
