d9d28cdc66b860843607dfbfc2ef935ebd740b46
Thanapoleiei <loveyouna089@gamil.com>
2020-03-22 00:12:10 +0700

    array

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState,useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [tasks,setTasks] = useState([
+    { 
+      id:1, name: "do homework"
+    },
+    {
+    id:2, name: "write node"
+    }
+  ])
+  useEffect( () => {
+
+  })
+  const renderTask = () => {
+    if (tasks && tasks.length)
+    return tasks.map((task,index)=>{
+        return(
+          <li key={index}> {task.id} : {task.name}</li>
+        )
+      })
+  else
+      return (<li>No task</li>)
+    }
   return (
-    <div>
-      
-  
+    <div >
+        <h1>Todo</h1>
+        <ul>{ renderTask() }</ul>
     </div>
   );
 }
-
 export default App;
